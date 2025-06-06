323d36af5323fb9f2e773c1b7dc53b2514ff7934
Jeidy Olaya <jeidkata@gmail.com>
2025-06-05 20:57:39 -0500

    Creo dos funciones dentro de la clase PurchaseProductTest para probar la ejecución de testng.xml y mirar si funcionan los listeners y me trae la data de la clase Data. Hasta aquí funciona el proyecto

--- src/test/java/com/globant/sauceProject/tests/PurchaseProductTest.java
+++ src/test/java/com/globant/sauceProject/tests/PurchaseProductTest.java
@@ -1,4 +1,24 @@
 package com.globant.sauceProject.tests;
 
+import com.globant.sauceProject.data.Data;
+import org.testng.annotations.Test;
+
+import java.util.logging.Logger;
+
 public class PurchaseProductTest {
+
+    public Logger logs = Logger.getLogger(LogoutTest.class.getName());
+
+    @Test(groups = {"dataLogin"}, description = "Inicio sesión", dataProvider = "loginData", dataProviderClass = Data.class)
+    public void testLogin (String username, String password){
+        logs.info("Probando Login con: ");
+        logs.info("Username: " + username + System.lineSeparator() +
+                "Contraseña: " + password + System.lineSeparator());
+    }
+
+    @Test(groups = {"example"})
+    public void funtionExample(){
+        logs.info("Hola ejemplo");
+    }
+
 }

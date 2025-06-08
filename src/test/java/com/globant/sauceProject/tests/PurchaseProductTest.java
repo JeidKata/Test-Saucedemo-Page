791889079237670465c0a09a6139dcea989b4039
Jeidy Olaya <jeidkata@gmail.com>
2025-06-07 22:54:38 -0500

    Instancio CartPage y uso sus métodos. Hasta aquí funciona bien el test.

--- src/test/java/com/globant/sauceProject/tests/PurchaseProductTest.java
+++ src/test/java/com/globant/sauceProject/tests/PurchaseProductTest.java
@@ -1,9 +1,7 @@
 package com.globant.sauceProject.tests;
 
 import com.globant.sauceProject.data.Data;
-import com.globant.sauceProject.pages.BasePage;
-import com.globant.sauceProject.pages.LoginPage;
-import com.globant.sauceProject.pages.ProductsPage;
+import com.globant.sauceProject.pages.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +18,7 @@ public class PurchaseProductTest {
     private BasePage base;
     private LoginPage login;
     private ProductsPage products;
+    private CartPage cart;
 
     @BeforeMethod
     public void setUp() {
@@ -31,9 +30,10 @@ public class PurchaseProductTest {
         base = new BasePage(driver);
         login = new LoginPage(driver);
         products = new ProductsPage(driver);
+        cart = new CartPage(driver);
     }
 
-    @Test(priority = 1, groups = {"dataLogin"}, description = "Inicio sesión", dataProvider = "loginData", dataProviderClass = Data.class)
+    @Test(groups = {"dataLogin"}, description = "Inicio sesión", dataProvider = "loginData", dataProviderClass = Data.class)
     public void testLogin (String username, String password) {
         logs.info("Probando Login con: ");
         logs.info("Username: " + username + System.lineSeparator() +
@@ -46,5 +46,7 @@ public class PurchaseProductTest {
         products.addThreeProductsToCart();
         products.clickShoppingCart();
         logs.info("Productos agregados al carrito y se ha hecho clic en el enlace del carrito de compras.");
+        cart.getCartItemCount();
+        cart.clickCheckoutButton();
     }
 }

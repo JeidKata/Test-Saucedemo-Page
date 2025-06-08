7d647f9cb6ab3e5fd750238c4dc6a159668e2308
Jeidy Olaya <jeidkata@gmail.com>
2025-06-07 23:07:05 -0500

    Creo CheckoutInformationPage con sus atributos y metodos.

---
+++ src/test/java/com/globant/sauceProject/pages/CheckoutInformationPage.java
@@ -0,0 +1,37 @@
+package com.globant.sauceProject.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.logging.Logger;
+
+public class CheckoutInformationPage extends BasePage{
+    private Logger log = Logger.getLogger(CheckoutInformationPage.class.getName());
+
+    @FindBy(id = "first-name")
+    private WebElement firstNameField;
+
+    @FindBy(id = "last-name")
+    private WebElement lastNameField;
+
+    @FindBy(id = "postal-code")
+    private WebElement postalCodeField;
+
+    @FindBy(id = "continue")
+    private WebElement continueButton;
+
+    public CheckoutInformationPage(WebDriver driver) {
+        super(driver);
+    }
+
+    /**
+     * Metodo para ingresar firstName, lastName y postCode de checkout.
+     */
+    public void enterCheckoutInformation(String firstName, String lastName, String postalCode) {
+        firstNameField.sendKeys(firstName);
+        lastNameField.sendKeys(lastName);
+        postalCodeField.sendKeys(postalCode);
+        log.info("Checkout information entered: " + firstName + " " + lastName + ", Postal Code: " + postalCode);
+    }
+}

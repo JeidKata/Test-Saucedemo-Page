297e81c0beadc09f3b692f7ddc5f7eaa756a90cb
Jeidy Olaya <jeidkata@gmail.com>
2025-06-07 22:53:11 -0500

    Creo CartPage con sus atributos y metodos.

--- src/test/java/com/globant/sauceProject/pages/CartPage.java
+++ src/test/java/com/globant/sauceProject/pages/CartPage.java
@@ -1,4 +1,48 @@
 package com.globant.sauceProject.pages;
 
-public class CartPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class CartPage extends BasePage{
+    private Logger logs = Logger.getLogger(CartPage.class.getName());
+
+    @FindBy(className = "cart_item")
+    private List<WebElement> cartItemsList;
+
+    @FindBy(id = "cart_contents_container")
+    private WebElement cartContentsContainer;
+
+    @FindBy(id = "checkout")
+    private WebElement checkoutButton;
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    /**
+     * Obtiene el número de elementos en el carrito.
+     * muestra El número de elementos actualmente en el carrito.
+     */
+    public void getCartItemCount() {
+        this.wait.until(ExpectedConditions.visibilityOf(cartContentsContainer));
+        int itemCount = cartItemsList.size();
+        if (itemCount > 0) {
+            this.logs.info("Número de productos en el carrito: " + itemCount);
+        } else {
+            this.logs.info("El carrito está vacío.");
+        }
+    }
+
+    /**
+     * Metodo para hacer clic en el botón de checkout.
+     */
+    public void clickCheckoutButton() {
+        checkoutButton.click();
+        this.logs.info("Hizo clic en el botón de checkout.");
+    }
 }

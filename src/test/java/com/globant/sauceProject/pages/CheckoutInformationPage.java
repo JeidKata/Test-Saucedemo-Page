f1a3e7cf89d5807eef232e765cefbdc77b21cdae
Jeidy Olaya <jeidkata@gmail.com>
2025-06-10 00:09:50 -0500

    Quito el logger, cambio de una función que ingresaba toda la información a c/u de las funciones tengan que ingresar un solo elemento. Agrego mensajes y tiempo de espera.

--- src/test/java/com/globant/sauceProject/pages/CheckoutInformationPage.java
+++ src/test/java/com/globant/sauceProject/pages/CheckoutInformationPage.java
@@ -3,11 +3,9 @@ package com.globant.sauceProject.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
-import java.util.logging.Logger;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CheckoutInformationPage extends BasePage{
-    private Logger log = Logger.getLogger(CheckoutInformationPage.class.getName());
 
     @FindBy(id = "first-name")
     private WebElement firstNameField;
@@ -26,20 +24,32 @@ public class CheckoutInformationPage extends BasePage{
     }
 
     /**
-     * Metodo para ingresar firstName, lastName y postCode de checkout.
+     * Metodos para ingresar firstName, lastName y postCode de checkout.
      */
-    public void enterCheckoutInformation(String firstName, String lastName, String postalCode) {
+    public void enterFirstName(String firstName) {
+        this.wait.until(ExpectedConditions.visibilityOf(firstNameField)).isDisplayed();
         firstNameField.sendKeys(firstName);
+        logs.info("Entered first name: " + firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        lastNameField.click();
         lastNameField.sendKeys(lastName);
+        logs.info("Entered last name: " + lastName);
+    }
+
+    public void enterPostalCode(String postalCode) {
+        postalCodeField.click();
         postalCodeField.sendKeys(postalCode);
-        log.info("Checkout information entered: " + firstName + " " + lastName + ", Postal Code: " + postalCode);
+        logs.info("Entered postal code: " + postalCode);
     }
 
     /**
      * Metodo para hacer clic en el botón de continuar.
      */
     public void clickContinueButton() {
+        this.wait.until(ExpectedConditions.elementToBeClickable(continueButton)).isDisplayed();
         continueButton.click();
-        log.info("Clicked on the continue button.");
+        logs.info("Clicked on the continue button.");
     }
 }

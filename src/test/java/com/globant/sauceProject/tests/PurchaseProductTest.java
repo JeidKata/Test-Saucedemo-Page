b69d3225b630857676a96cbe273eaf17f8e8ee26
Jeidy Olaya <jeidkata@gmail.com>
2025-06-09 23:34:13 -0500

    Cambio toda la estructura con el fin de que todo lo herede de BaseTest. Pruebo y funciona, sin embargo, no muestra los mensajes del LOGGER.

--- src/test/java/com/globant/sauceProject/tests/PurchaseProductTest.java
+++ src/test/java/com/globant/sauceProject/tests/PurchaseProductTest.java
@@ -1,74 +1,48 @@
 package com.globant.sauceProject.tests;
 
-import com.globant.sauceProject.data.Data;
 import com.globant.sauceProject.pages.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.util.logging.Logger;
-
 public class PurchaseProductTest extends BaseTest{
-
-    public Logger logs = Logger.getLogger(LogoutTest.class.getName());
-    private static final String URL = "https://www.saucedemo.com/";
-    private WebDriver driver;
-    private BasePage base;
-    private LoginPage login;
-    private ProductsPage products;
-    private CartPage cart;
-    private CheckoutInformationPage checkoutInformation;
-    private CheckoutOverviewPage checkoutOverview;
-
-    @BeforeMethod
-    public void setUp() {
-        logs.info("Setting up the test environment.");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(URL);
-        base = new BasePage(driver);
-        login = new LoginPage(driver);
-        products = new ProductsPage(driver);
-        cart = new CartPage(driver);
-        checkoutInformation = new CheckoutInformationPage(driver);
-        checkoutOverview = new CheckoutOverviewPage(driver);
-    }
-
     @Parameters ({"username", "password", "firstName", "lastName", "postalCode"})
-    @Test
+    @Test(description = "Verifica el flujo de compra completo.")
 //            (groups = {"dataLogin"}, description = "Inicio sesión",dataProvider = "loginData", dataProviderClass = Data.class)
     public void testLogin (String username, String password, String firstName, String lastName, String postalCode) {
-        logs.info("Probando Login con: ");
-        logs.info("Username: " + username + System.lineSeparator() +
+        BasePage base = new BasePage(driver);
+        LoginPage login = new LoginPage(driver);
+        ProductsPage products = new ProductsPage(driver);
+        CartPage cart = new CartPage(driver);
+        CheckoutInformationPage checkoutInformation = new CheckoutInformationPage(driver);
+        CheckoutOverviewPage checkoutOverview = new CheckoutOverviewPage(driver);
+        CheckoutCompletePage checkoutComplete = new CheckoutCompletePage(driver);
+        logger.info("Probando Login con: ");
+        logger.info("Username: " + username + System.lineSeparator() +
                 "Contraseña: " + password + System.lineSeparator());
         base.getLogo();
         login.enterUsername(username);
         login.enterPassword(password);
         login.clickLoginButton();
-        logs.info("Probando agregar productos al carrito.");
+        logger.info("Probando agregar productos al carrito.");
         products.addThreeProductsToCart();
         products.clickShoppingCart();
-        logs.info("Productos agregados al carrito y se ha hecho clic en el enlace del carrito de compras.");
-        cart.getCartItemCount();
+        logger.info("Productos agregados al carrito y se ha hecho clic en el enlace del carrito de compras.");
+        Assert.assertTrue(cart.getCartItemCount() > 0, "El carrito debería contener al menos un producto.");
         cart.clickCheckoutButton();
         checkoutInformation.enterFirstName(firstName);
         checkoutInformation.enterLastName(lastName);
         checkoutInformation.enterPostalCode(postalCode);
-//        checkoutInformation.enterCheckoutInformation(firstName, lastName, postalCode);
         checkoutInformation.clickContinueButton();
-//        checkoutOverview.isCheckoutOverviewPageLoaded();
+        checkoutOverview.isCheckoutOverviewPageLoaded();
         checkoutOverview.clickFinishButton();
-
+        checkoutComplete.isCheckoutComplete();
+        checkoutComplete.clickBackHomeButton();
     }
 //    @AfterMethod
 //    public void testCheckout(String firstName, String lastName, String postalCode) {
-//        logs.info("Probando Checkout con: ");
-//        logs.info("First Name: " + firstName + System.lineSeparator() +
+//        logger.info("Probando Checkout con: ");
+//        logger.info("First Name: " + firstName + System.lineSeparator() +
 //                "Last Name: " + lastName + System.lineSeparator() +
 //                "Postal Code: " + postalCode + System.lineSeparator());
 //        checkoutInformation.enterCheckoutInformation(firstName, lastName, postalCode);
